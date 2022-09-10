5ad16bb86c1cd6f4868780539bb9d47d223eb66d
Cristiano Grandi <cristigrandi14@gmail.com>
2022-09-10 12:59:11 -0300

    Colorir botões

--- src/components/NumberButtons.tsx
+++ src/components/NumberButtons.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement, memo } from "react"
 
 const NumberButtons: React.FC<{ keySymbol: string, key: number, handleClick: (keySymbol: string) => void }> = ({ keySymbol, key, handleClick }): ReactElement => {
     return (
-        <button className='keyboard-key' key={key} onClick={() => handleClick(keySymbol)}>
+        <button className={`keyboard-key ${Number.isInteger(Number(keySymbol)) ? 'number-key' : 'symbol-key'}`} key={key} onClick={() => handleClick(keySymbol)}>
             {keySymbol}
         </button>
     )
